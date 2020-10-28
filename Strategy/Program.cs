5949d4185c87c733afab218f0f5eeefdf8fab75e
Vinogradov, Nikolay <NVinogradov@inwk.com>
2020-10-28 19:18:02 +0300

    Create Project

---
+++ Strategy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Strategy!");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
